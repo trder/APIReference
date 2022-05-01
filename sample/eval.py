169caaef8b366c9650d6eda92940cc9fde62e1be
Crypto Trader <103327499+trder@users.noreply.github.com>
2022-05-01 18:37:46 +0800

    Update eval.py

--- sample/eval.py
+++ sample/eval.py
@@ -3,7 +3,11 @@
 #启动模拟交易
 import sys
 
-gpus = sys.argv[1]
+def load_trading_system():
+    '''
+    载入交易系统
+    '''
+    pass
 
 if __name__ == '__main__':
     #获取参数个数
@@ -11,5 +15,14 @@ if __name__ == '__main__':
     if nargv < 2:
         print("请输入系统名")
         return
+    #获取交易系统名
+    trading_system_name = sys.argv[1]
     #载入交易系统
-    load_trading_system()
+    trading_system = load_trading_system(trading_system_name)
+    if not trading_system:
+        print("交易系统载入失败")
+        return
+    #评估交易系统
+    eval_result = eval_trading_system(trading_system)
+    #输出评估结果
+    print(eval_result)
