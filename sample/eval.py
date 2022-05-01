5a5e33f5e35bdb491dd3950e3b51e96bf6b56e50
Crypto Trader <103327499+trder@users.noreply.github.com>
2022-05-01 18:28:50 +0800

    Update eval.py

--- sample/eval.py
+++ sample/eval.py
@@ -1,6 +1,12 @@
 #启动模拟交易
 #输入指令：python eval.py donchian
 #启动模拟交易
+import sys
+
+gpus = sys.argv[1]
 
 if __name__ == '__main__':
-    pass
+    #获取参数个数
+    nargv = len(sys.argv)
+    #载入交易系统
+    load_trading_system()
